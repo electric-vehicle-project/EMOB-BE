967d5534c2c2ad0495313ea5f13d223620e69ec9
minhvuong09090808@gmail.com <minhvuong09090808@gmail.com>
2025-09-21 11:54:15 +0700

    config

--- src/main/java/com/example/emob/EmobApplication.java
+++ src/main/java/com/example/emob/EmobApplication.java
@@ -1,13 +1,18 @@
 package com.example.emob;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Minhvuong0909", version = "2.5", description = "Information"))
+@SecurityScheme(name = "api", scheme = "bearer", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
 public class EmobApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EmobApplication.class, args);
 	}
-
 }
