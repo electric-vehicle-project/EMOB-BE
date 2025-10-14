195bb50a43a48e50c57e91c68820103e66eb2c63
minhvuong09090808@gmail.com <minhvuong09090808@gmail.com>
2025-10-14 13:52:24 +0700

    fix: import set

--- src/main/java/com/example/emob/entity/VehicleUnit.java
+++ src/main/java/com/example/emob/entity/VehicleUnit.java
@@ -7,6 +7,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Set;
 import java.util.UUID;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
