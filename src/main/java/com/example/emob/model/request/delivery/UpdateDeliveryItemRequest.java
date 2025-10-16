bac1843e404b9b9712d58ae4d09178174e0d6da0
minhvuong09090808@gmail.com <minhvuong09090808@gmail.com>
2025-10-17 01:33:59 +0700

    feat: add validation constraints for delivery requests and items

--- src/main/java/com/example/emob/model/request/delivery/UpdateDeliveryItemRequest.java
+++ src/main/java/com/example/emob/model/request/delivery/UpdateDeliveryItemRequest.java
@@ -1,5 +1,7 @@
 package com.example.emob.model.request.delivery;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
@@ -11,6 +13,8 @@ import java.util.UUID;
 @NoArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UpdateDeliveryItemRequest {
+    @NotNull(message = "Vehicle ID cannot be null")
     UUID vehicleId;
+    @NotBlank(message = "Remarks cannot be blank")
     String remarks;
 }
