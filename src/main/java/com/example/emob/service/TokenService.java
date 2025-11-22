d0fd8a92a1a8b6b227223c93035ebe478fe960af
TruongV2905 <truong0345218921@gmail.com>
2025-11-22 17:30:12 +0700

    fix: Update expiration time for access token and correct API key variable in application configuration

--- src/main/java/com/example/emob/service/TokenService.java
+++ src/main/java/com/example/emob/service/TokenService.java
@@ -97,7 +97,7 @@ public class TokenService {
         .claim("token_type", "access") // phân biệt loại
         .claim("roles", "ROLE_" + account.getRole()) // roles nếu bạn cần
         .issuedAt(Date.from(now)) // iat
-        .expiration(Date.from(now.plus(15, java.time.temporal.ChronoUnit.MINUTES))) // 15'
+        .expiration(Date.from(now.plus(60, java.time.temporal.ChronoUnit.MINUTES))) // 15'
         .signWith(getSignKey(), Jwts.SIG.HS256) // KHÔNG dùng chung key với refresh
         .compact();
   }
